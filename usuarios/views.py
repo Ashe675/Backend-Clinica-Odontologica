8365175af0e693391a05814bc135e08d4671020a
Jose Cerrato <josecerrato675@gmail.com>
2024-04-09 23:48:26 -0600

    Probema de login solucionado

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,8 +4,11 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def signin(request):
     if request.method == 'GET':
         context = {
@@ -13,7 +16,13 @@ def signin(request):
         }
         return render(request,'signin.html', context)
     else:
-        user = authenticate(request, username = request.POST['username'], password=request.POST['password'])
+        data = json.loads(request.body)
+        print(data)
+        username = data.get('username')
+        password = data.get('password')
+        print(username)
+        print(password)
+        user = authenticate(request, username = username, password = password)
         if user is None:
             context = {
             'form': AuthenticationForm,
